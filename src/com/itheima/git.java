2be10192ab9b320b4cda670c239b37a669f5233f
CaoYbyn <1210274965@qq.com>
2018-10-28 18:32:54 +0800

    first commit

---
+++ src/com/itheima/git.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class git {
+    public static void main(String[] args) {
+
+        System.out.println("github,1");
+        System.out.println("github,1");
+        System.out.println("github,1");
+    }
+}
